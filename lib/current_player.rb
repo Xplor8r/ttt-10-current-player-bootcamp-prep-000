c7787a29bfa48a20d3e8a132827da05012bf0b0e
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-20 01:52:46 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count % 2 == 0 || turn_count == 0
+  if turn_count(board).length % 2 == 0 || turn_count(board).length == 0
+    puts "X"
   else
+    puts "O"
   end
 end
